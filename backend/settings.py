6f547b67e9eeb1a2b6342202437248ea9f72499d
Parmod-makpower <it@makpowerindia.com>
2025-07-30 11:56:59 +0530

    2replace frontend url

--- backend/settings.py
+++ backend/settings.py
@@ -127,10 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:5173",  # 👈 React dev server
         "https://frontend-599c.onrender.com"
 ]
 
