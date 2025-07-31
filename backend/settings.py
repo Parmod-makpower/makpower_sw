dee093ed3a66f64009194a2fef231b9dddb76b56
Parmod-makpower <it@makpowerindia.com>
2025-07-31 09:42:32 +0530

    interval 2

--- backend/settings.py
+++ backend/settings.py
@@ -151,7 +151,10 @@ SIMPLE_JWT = {
 
 
 
-GOOGLE_CREDS = BASE_DIR / "credentials.json"
+# GOOGLE_CREDS = BASE_DIR / "credentials.json"
+
+GOOGLE_CREDS = os.environ.get("GOOGLE_CREDS_PATH", "/etc/secrets/credentials.json")
+
 
 # SHEET_ID = "1_UzX_ZU5RvCxG6rOJs0V9jwpvEqoFeCSCO6A6ZLgI4s"
 SHEET_ID_NEW = "1fiDkMYjfXSptaiDnYJfdhXI69J5luXS9M-RR-cVpVYI"
