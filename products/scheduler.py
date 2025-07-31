6808b41ea8a4354eff85265fc2b682d102f32f35
Parmod-makpower <it@makpowerindia.com>
2025-07-31 09:53:09 +0530

    interval 3

--- products/scheduler.py
+++ products/scheduler.py
@@ -5,5 +5,5 @@ from .sync import  sheet_to_db
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(sheet_to_db, 'interval', minutes=1)
+    scheduler.add_job(sheet_to_db, 'interval', minutes=30)
     scheduler.start()
