49105c266e175556bd16ee7a6f8be8e9c26c11c0
Parmod <it@makpowerindia.com>
2025-11-15 09:35:36 +0530

    15ov

--- orders/migrations/0005_crmverifiedorderitem_is_rejected.py
+++ orders/migrations/0005_crmverifiedorderitem_is_rejected.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orders', '0004_dispatchorder'),
+        ('orders', '0001_initial'),
     ]
 
     operations = [
