2d403ef35172e657aca28d2999220bf2bce62075
Parmod <it@makpowerindia.com>
2025-12-17 16:07:50 +0530

    17dec6

--- otherinfo/apps.py
+++ otherinfo/apps.py
@@ -1,5 +1,13 @@
+
 from django.apps import AppConfig
 
-class OtherinfoConfig(AppConfig):
+
+class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'otherinfo'
+
+    def ready(self):
+    
+        # 🕒 Scheduler को भी run होने दो जैसा पहले था
+        from .scheduler import start
+        start()
--- otherinfo/scheduler.py
+++ otherinfo/scheduler.py
@@ -3,6 +3,6 @@ from otherinfo.sync import sync_sampling_sheet
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(sync_sampling_sheet, 'interval', minutes=30)
+    scheduler.add_job(sync_sampling_sheet, 'interval', minutes=10)
 
     scheduler.start()
