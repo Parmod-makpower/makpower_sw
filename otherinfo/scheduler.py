4d8f74e1b460a7ee70c646d6de8528fcbf3bab29
Parmod <it@makpowerindia.com>
2026-03-10 14:57:51 +0530

    10march1

--- otherinfo/scheduler.py
+++ otherinfo/scheduler.py
@@ -5,7 +5,7 @@ def start():
     scheduler = BackgroundScheduler()
 
     # Sampling → 4 hours
-    scheduler.add_job(sync_sampling_sheet, 'interval', minutes=2)
+    scheduler.add_job(sync_sampling_sheet, 'interval', hours=3)
 
     # Not In Stock → 6 hours
     scheduler.add_job(sync_not_in_stock, 'interval', hours=4)
